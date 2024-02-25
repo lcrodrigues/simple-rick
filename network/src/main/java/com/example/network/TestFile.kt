77bca808d083a61c39733fe26117d3872fac003e
Leonardo Rodrigues <leonardo.rodrigues@poatek.com>
2024-02-25 15:38:07 -0300

    Project created + network module

---
+++ network/src/main/java/com/example/network/TestFile.kt
@@ -0,0 +1,9 @@
+package com.example.network
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TestFile() {
+    Text("Hello from the other module.")
+}
